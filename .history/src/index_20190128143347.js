40545c147d50c2334bc864efc13ab37e7b088946
Uvizius <connerchevaillier@gmail.com>
2019-01-29 13:58:14 -0700

    simple message list practice

---
+++ .history/src/index_20190128143347.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import faker from 'faker';
+import CommentDetail from './CommentDetail';
+import * as serviceWorker from './serviceWorker';
+
+
+const App = () => {
+   return (
+      <div className="ui container comments">
+         <CommentDetail author='sam' />
+         <CommentDetail author='david' />
+         <CommentDetail author='chris' />
+         <CommentDetail author='max' />
+         <CommentDetail author='davey' />
+         <CommentDetail author='rex' />
+      </div>
+
+   );
+}
+ReactDOM.render(<App />, document.querySelector('#root')
+);
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
--- src/index.js
+++ src/index.js
@@ -1,11 +1,68 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import faker from 'faker';
+import CommentDetail from './CommentDetail';
+import CardTemplateForCommentDetail from './CardTemplateForCommentDetail';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
+const App = () => {
+   return (
+      <div className="ui container comments">
+         <CardTemplateForCommentDetail>
+            <CommentDetail
+               author='sam'
+               timeAgo="Today at 4:30pm"
+               commentPost="have a great day"
+               avatar={faker.image.avatar()} />
+         </CardTemplateForCommentDetail>
+
+         <CardTemplateForCommentDetail>
+            <CommentDetail
+               author='david'
+               timeAgo="Today at 4:07am"
+               commentPost="have a great day"
+               avatar={faker.image.avatar()} />
+
+         </CardTemplateForCommentDetail>
+
+         <CardTemplateForCommentDetail>
+            <CommentDetail
+               author='chris'
+               timeAgo="Today at 4:01am"
+               commentPost="have a great day"
+               avatar={faker.image.avatar()} />
+
+         </CardTemplateForCommentDetail >
+
+         <CardTemplateForCommentDetail >
+            <CommentDetail
+               author='max'
+               timeAgo="Today at 3:00"
+               commentPost="have a great day"
+               avatar={faker.image.avatar()} />
+         </CardTemplateForCommentDetail>
+
+         <CardTemplateForCommentDetail >
+            <CommentDetail
+               author='davey'
+               timeAgo="Today at 2:30"
+               commentPost="have a great day"
+               avatar={faker.image.avatar()} />
+         </CardTemplateForCommentDetail>
+         <CardTemplateForCommentDetail >
+            <CommentDetail
+               author='rex'
+               timeAgo="Today at 1:30"
+               commentPost="have a great day"
+               avatar={faker.image.avatar()} />
+         </CardTemplateForCommentDetail>
+      </div >
+
+   );
+}
+ReactDOM.render(<App />, document.querySelector('#root')
+);
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
